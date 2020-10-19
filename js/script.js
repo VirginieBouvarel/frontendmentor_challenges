206497e36333525fd4ddb33702cd4b3a20ff18f8
Virginie <vbouvarel@lilo.org>
2020-10-19 17:46:28 +0200

    toggleNav js wip

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 "use strict";
+
+const burger = document.querySelector('#burger');
+const nav = document.querySelector('#nav');
+console.log(nav);
+
+console.log(burger);
+
+burger.addEventListener('click', toggleNav);
+
+
+function toggleNav() {
+    
+    if (burger.getAttribute('src') === 'img/icon-hamburger.svg') {
+        burger.setAttribute('src', 'img/icon-close.svg');
+        nav.style.display = "block";
+    }else {
+        burger.setAttribute('src', 'img/icon-hamburger.svg');
+        nav.style.display = "none";
+    }
+  
+}
