1ebb0438e25e2fabeae1e4ec65bf6c6527313ef7
Virginie <vbouvarel@lilo.org>
2020-10-23 11:01:28 +0200

    animation nav ok

--- js/script.js
+++ js/script.js
@@ -2,21 +2,18 @@
 
 const burger = document.querySelector('#burger');
 const nav = document.querySelector('#nav');
-console.log(nav);
 
-console.log(burger);
 
 burger.addEventListener('click', toggleNav);
 
 
 function toggleNav() {
+    nav.classList.toggle('nav-mobile--show');
     
     if (burger.getAttribute('src') === 'img/icon-hamburger.svg') {
         burger.setAttribute('src', 'img/icon-close.svg');
-        nav.style.display = "block";
     }else {
         burger.setAttribute('src', 'img/icon-hamburger.svg');
-        nav.style.display = "none";
     }
   
 }
