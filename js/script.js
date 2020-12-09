9b96581459d735bc01da787a470b22338926eafc
Virginie <vbouvarel@lilo.org>
2020-12-09 16:42:44 +0100

    ajout js ouverture des list__item

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 "use strict";
+
+const items = document.querySelectorAll('.list__item');
+
+
+for (let i = 0; i < items.length; i++) {
+    items[i].addEventListener('click', () => {
+        for (let j = 0; j < items.length; j++) {
+            items[j].classList.remove('open');
+        }
+        items[i].classList.add('open');
+    });
+}
+
+
+ 
