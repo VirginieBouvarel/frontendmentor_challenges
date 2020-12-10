a0a05c033b38b000693ba620d82997ed693a596c
Virginie <vbouvarel@lilo.org>
2020-12-10 15:27:50 +0100

    refacto js

--- js/script.js
+++ js/script.js
@@ -5,9 +5,7 @@ const items = document.querySelectorAll('.list__item');
 
 for (let i = 0; i < items.length; i++) {
     items[i].addEventListener('click', () => {
-        for (let j = 0; j < items.length; j++) {
-            items[j].classList.remove('open');
-        }
+        document.querySelector('.open').classList.remove('open');
         items[i].classList.add('open');
     });
 }
